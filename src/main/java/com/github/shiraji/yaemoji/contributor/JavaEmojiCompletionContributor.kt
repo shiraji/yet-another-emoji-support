4b195a7456eb52583abf104d8ffeff5346cf9c21
shiraji <isogai.shiraji@gmail.com>
2019-05-31 00:51:23 +0900

    Extract Java's emoji contributor to new class

--- src/main/java/com/github/shiraji/yaemoji/contributor/JavaEmojiCompletionContributor.kt
+++ src/main/java/com/github/shiraji/yaemoji/contributor/JavaEmojiCompletionContributor.kt
@@ -5,11 +5,12 @@ import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiJavaToken
 
-class EmojiCompletionContributor : CompletionContributor() {
+class JavaEmojiCompletionContributor : CompletionContributor() {
     init {
         val provider = EmojiCompletionProvider()
 
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(PsiComment::class.java), provider)
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(PsiJavaToken::class.java), provider)
     }
 }
