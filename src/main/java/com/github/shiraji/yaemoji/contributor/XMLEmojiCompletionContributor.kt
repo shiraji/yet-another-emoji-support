e77cec09943723c44d98f1e69f6abcb3c4007c13
shiraji <isogai.shiraji@gmail.com>
2019-06-06 03:16:42 +0900

    Fix ClassCastException

--- src/main/java/com/github/shiraji/yaemoji/contributor/XMLEmojiCompletionContributor.kt
+++ src/main/java/com/github/shiraji/yaemoji/contributor/XMLEmojiCompletionContributor.kt
@@ -2,15 +2,12 @@ package com.github.shiraji.yaemoji.contributor
 
 import com.github.shiraji.yaemoji.utils.or
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.patterns.PsiElementPattern
-import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlComment
 import com.intellij.psi.xml.XmlText
 import com.intellij.psi.xml.XmlToken
 
 class XMLEmojiCompletionContributor : EmojiCompletionContributor() {
-    override val place =
-            PlatformPatterns.psiElement().inside(XmlText::class.java)
-                    .or(PlatformPatterns.psiElement().inside(XmlToken::class.java))
-                    .or(PlatformPatterns.psiElement().inside(XmlComment::class.java)) as PsiElementPattern.Capture<PsiElement>
+    override val place = PlatformPatterns.psiElement().inside(XmlText::class.java)
+        .or(PlatformPatterns.psiElement().inside(XmlToken::class.java))
+        .or(PlatformPatterns.psiElement().inside(XmlComment::class.java))
 }
--- src/main/java/com/github/shiraji/yaemoji/contributor/YAMLEmojiCompletionContributor.kt
+++ src/main/java/com/github/shiraji/yaemoji/contributor/YAMLEmojiCompletionContributor.kt
@@ -2,12 +2,10 @@ package com.github.shiraji.yaemoji.contributor
 
 import com.github.shiraji.yaemoji.utils.or
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.patterns.PsiElementPattern
-import com.intellij.psi.PsiElement
 import org.jetbrains.yaml.psi.YAMLQuotedText
 import org.jetbrains.yaml.psi.YAMLValue
 
 class YAMLEmojiCompletionContributor : EmojiCompletionContributor() {
     override val place = PlatformPatterns.psiElement().inside(YAMLQuotedText::class.java)
-            .or(PlatformPatterns.psiElement().inside(YAMLValue::class.java)) as PsiElementPattern.Capture<PsiElement>
+            .or(PlatformPatterns.psiElement().inside(YAMLValue::class.java))
 }
