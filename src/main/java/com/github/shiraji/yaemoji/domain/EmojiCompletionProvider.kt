738a5ade636a127e5801e088f6e4d448d9bf6879
shiraji <isogai.shiraji@gmail.com>
2019-06-22 02:38:44 +0900

    Change end offset to use position element
    e.g. String::from("<caret>") is valid because there is colon out side of
    string. This commit now checks position's element.

--- src/main/java/com/github/shiraji/yaemoji/domain/EmojiCompletionProvider.kt
+++ src/main/java/com/github/shiraji/yaemoji/domain/EmojiCompletionProvider.kt
@@ -14,8 +14,7 @@ class EmojiCompletionProvider : CompletionProvider<CompletionParameters>() {
         if (parameters.editor.isOneLineMode) return
         // -1 because it should check with text's index
         val start = parameters.editor.caretModel.currentCaret.offset - 1
-        // no -1 because downTo method is inclusive + the plugin need index of the text
-        val end = parameters.editor.caretModel.currentCaret.visualLineStart
+        val end = maxOf(parameters.position.textRange.startOffset - 1, 0)
         val text = parameters.editor.document.text
         var colonPosition = -1
         loop@ for (index in start downTo end) {
