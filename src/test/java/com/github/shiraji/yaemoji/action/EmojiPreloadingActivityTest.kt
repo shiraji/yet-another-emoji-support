3d505b1a514a9a0ff884a7e49bfc7a32c78d2207
shiraji <isogai.shiraji@gmail.com>
2019-06-23 00:16:40 +0900

    Add preloading tests

---
+++ src/test/java/com/github/shiraji/yaemoji/action/EmojiPreloadingActivityTest.kt
@@ -0,0 +1,33 @@
+package com.github.shiraji.yaemoji.action
+
+import com.github.shiraji.yaemoji.domain.EmojiDataManager
+import com.intellij.openapi.progress.EmptyProgressIndicator
+import com.intellij.testFramework.builders.ModuleFixtureBuilder
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class EmojiPreloadingActivityTest : CodeInsightFixtureTestCase<ModuleFixtureBuilder<*>>() {
+
+    @BeforeEach
+    fun beforeEach() {
+        setUp()
+        EmojiDataManager.emojiList.clear()
+    }
+
+    @AfterEach
+    fun afterEach() {
+        EmojiDataManager.emojiList.clear()
+        tearDown()
+    }
+
+    @Test
+    fun `Should load emojis`() {
+        EmojiPreloadingActivity().preload(EmptyProgressIndicator())
+
+        assertThat(EmojiDataManager.emojiList).hasSize(1719)
+        assertThat(EmojiDataManager.emojiList[100].id).isEqualTo(101)
+    }
+}
